9e76de762225b50e0e46b12ac134a200343bb256
koichaman <k8o2u9@gmail.com>
2019-12-20 01:46:58 +0900

    bug fix of camera pose, add camera following

--- src/making_map.cpp
+++ src/making_map.cpp
@@ -10,13 +10,26 @@
 
 #define SPHERE_RAD 0.1
 #define ARROW_SCALE 1.0
+#define BACK_SCALE 3.0
+#define CAMERA_ADDITINAL_Z 1.0
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointXYZ cameraPt(0,0,0);
 tf::Transform cameraTf;
-tf::Vector3 cameraDir(1,0,0);
+tf::Vector3 cameraViewDir(1,0,0);
+tf::Vector3 cameraUpDir(0,0,1);
 tf::Vector3 UNIT_VECTOR_X(1,0,0);
-pcl::PointXYZ cameraArrowPt(0,0,0);
+tf::Vector3 UNIT_VECTOR_Z(0,0,1);
+tf::Vector3 tfCameraArrowPt(0,0,0);
+pcl::PointXYZ pclCameraArrowPt(0,0,0);
+
+pcl::PointXYZ tfVectorToPCL(tf::Vector3 tfVec){
+    pcl::PointXYZ output;
+    output.x = tfVec.getX();
+    output.y = tfVec.getY();
+    output.z = tfVec.getZ();
+    return output;
+}
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
     // Create a container for the data.
@@ -30,10 +43,10 @@ void pose_cb (const geometry_msgs::PoseStamped& msg){
     cameraPt.x = cameraTf.getOrigin().getX();
     cameraPt.y = cameraTf.getOrigin().getY();
     cameraPt.z = cameraTf.getOrigin().getZ();
-    cameraDir = cameraTf * UNIT_VECTOR_X;
-    cameraArrowPt.x = cameraPt.x + ARROW_SCALE * cameraDir.getX();
-    cameraArrowPt.y = cameraPt.y + ARROW_SCALE * cameraDir.getY();
-    cameraArrowPt.z = cameraPt.z + ARROW_SCALE * cameraDir.getZ();
+    tfCameraArrowPt = cameraTf * (ARROW_SCALE * UNIT_VECTOR_X);
+    pclCameraArrowPt = tfVectorToPCL(tfCameraArrowPt);
+    cameraViewDir = (1/ARROW_SCALE) * (tfCameraArrowPt - cameraTf.getOrigin());
+    cameraUpDir = cameraTf * UNIT_VECTOR_Z - cameraTf.getOrigin();
 }
 
 int main (int argc, char** argv){
@@ -49,20 +62,30 @@ int main (int argc, char** argv){
 
     ros::spinOnce();
     viewer.addPointCloud(cloud);
-    viewer.addArrow(cameraArrowPt, cameraPt, 0, 1, 0, false);
+    viewer.addArrow(pclCameraArrowPt, cameraPt, 0, 1, 0, false);
     viewer.addSphere(cameraPt, SPHERE_RAD, 0, 1, 0);
     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2);
-    viewer.addCoordinateSystem();
-    viewer.setCameraPosition(-20, 0, 5, 0, 0, 0, 1, 0, 3);
+    viewer.addCoordinateSystem(0.1);
+    viewer.setCameraPosition(
+        cameraPt.x-BACK_SCALE*cameraViewDir.getX(),
+        cameraPt.y-BACK_SCALE*cameraViewDir.getY(),
+        cameraPt.z-BACK_SCALE*cameraViewDir.getZ() + CAMERA_ADDITINAL_Z,
+        cameraPt.x, cameraPt.y, cameraPt.z, 0, 0, 1);
+    // viewer.setCameraPosition(-5,0,10,0,0,0,1,0,3);
     viewer.spinOnce();
 
     ros::Rate r(10); // 10 hz
     while(ros::ok()){
         ros::spinOnce();
         viewer.removeShape("arrow");
-        viewer.addArrow(cameraArrowPt, cameraPt, 0, 1, 0, false);
+        viewer.addArrow(pclCameraArrowPt, cameraPt, 0, 1, 0, false);
         viewer.updatePointCloud(cloud);
         viewer.updateSphere(cameraPt, SPHERE_RAD, 0, 1, 0);
+        viewer.setCameraPosition(
+            cameraPt.x-BACK_SCALE*cameraViewDir.getX(),
+            cameraPt.y-BACK_SCALE*cameraViewDir.getY(),
+            cameraPt.z-BACK_SCALE*cameraViewDir.getZ() + CAMERA_ADDITINAL_Z,
+            cameraPt.x, cameraPt.y, cameraPt.z, 0, 0, 1);
 	    viewer.spinOnce();
         r.sleep();
     }
